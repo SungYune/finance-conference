994201c967ea304e9a34380991404e9204ab3be6
orange-fritters <mindongss@snu.ac.kr>
2023-01-02 23:26:12 +0900

    updated script file

--- tests/mindong/mindong_test.sh
+++ tests/mindong/mindong_test.sh
@@ -8,40 +8,26 @@ venv/bin/python train.py \
   --data_path data/forex/t_note.csv \
   --accelerator cpu \
   --num_layers 5 \
-  --hidden_size 512 \
-  --epoch 10 \
+  --hidden_size 128 \
+  --epoch 2 \
   --seq_len 96 \
   --pred_len 31 \
   --label_len 10 \
   --learning_rate 0.0001 \
   --weight_decay 0.0001 \
   --scale False
-#
-#venv/bin/python train.py \
-#  --model_id n_linear \
-#  --model_type nlinear \
-#  --model_arch_desc test \
-#  --accelerator mps \
-#  --data_path data/forex/t_note.csv \
-#  --epoch 10 \
-#  --seq_len 730 \
-#  --pred_len 30 \
-#  --label_len 10 \
-#  --learning_rate 0.0001 \
-#  --weight_decay 0.0001 \
-#  --scale True
 
-# arima not working
-#venv/bin/python train.py \
-#  --model_id arima \
-#  --model_type arima \
-#  --model_arch_desc test \
-#  --data_path data/forex/t_note.csv \
-#  --accelerator cpu \
-#  --epoch 10 \
-#  --seq_len 730 \
-#  --pred_len 30 \
-#  --label_len 10 \
-#  --learning_rate 0.0001 \
-#  --weight_decay 0.0001 \
-
+venv/bin/python train.py \
+  --model_id n_linear \
+  --model_type nlinear \
+  --model_arch_desc test \
+  --accelerator mps \
+  --data_path data/forex/t_note.csv \
+  --epoch 2 \
+  --seq_len 730 \
+  --pred_len 30 \
+  --label_len 10 \
+  --learning_rate 0.0001 \
+  --weight_decay 0.0001 \
+  --scale True \
+  --run_tensorboard True
---
+++ tests/yoon/yoon_test.sh
@@ -0,0 +1,22 @@
+cd ../../
+pwd
+
+for learning_rate in 0.001 0.0001 0.00001
+do
+venv/bin/python train.py \
+  --model_id lstm \
+  --model_type lstm \
+  --model_arch_desc test \
+  --data_path data/forex/t_note.csv \
+  --accelerator cpu \
+  --num_layers 5 \
+  --hidden_size 128 \
+  --epoch 10 \
+  --seq_len 96 \
+  --pred_len 31 \
+  --label_len 10 \
+  --learning_rate $learning_rate \
+  --weight_decay 0.0001 \
+  --scale False \
+  --run_tensorboard True
+done
